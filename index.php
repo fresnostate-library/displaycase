5be3383fdaf5ba4fa55506c91b6198e796ef132b
Steven Orr <steve.a.orr@gmail.com>
2024-10-17 15:16:15 -0700

    Add surrounding comments for custom code sections

--- index.php
+++ index.php
@@ -152,16 +152,21 @@ for($i = 0; $i < $size; $i++) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel = "stylesheet" href = "<?php echo $scriptPathFromWebRoot; ?>assets/displaycase-styles.css">
 
+    <!-- ## START: Custom CSS Area ## -->
     <?php include 'assets/includes/custom-css.htm'; ?>
+    <!-- ## END: Custom CSS Area ## -->
 
+    <!-- ## START: Custom JavaScript Area ## -->
     <?php include 'assets/includes/custom-js.htm'; ?>
-
+    <!-- ## START: Custom JavaScript Area ## -->
 </head>
 <body>
 
-    <?php if (!$hideHeader): ?>
+    <!-- ## START: Custom Header Area ## -->
+     <?php if (!$hideHeader): ?>
     <?php include 'assets/includes/header.php'; ?>
     <?php endif; ?>
+    <!-- ## END: Custom Header Area ## -->
 
     <div id="top" class="container">
 
@@ -220,9 +225,11 @@ for($i = 0; $i < $size; $i++) {
 
     </div> <!-- // .container -->
 
-    <?php if (!$hideFooter): ?>
+    <!-- ## START: Custom Footer Area ## -->
+     <?php if (!$hideFooter): ?>
     <?php include 'assets/includes/footer.php'; ?>
     <?php endif; ?>
+    <!-- ## END: Custom Footer Area ## -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
